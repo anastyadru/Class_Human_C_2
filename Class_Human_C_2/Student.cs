4e4eeb91c464f989e5cf5e532f814c4c90c6ae84
Nastia <adruts@mail.ru>
2023-05-30 22:11:48 +0300

    https://github.com/anastyadru/Class_Human_C_2.git

--- Class_Human_C_2/Student.cs
+++ Class_Human_C_2/Student.cs
@@ -38,6 +38,48 @@ namespace Class_Human_C_2
         {
             return $"{base.Print()}, Факультет: {Faculty}, Курс: {Course}, Группа: {Group}";
         }
+
+        public override void Edit()
+        {
+            base.Edit();
+            var fieldToEdit = int.Parse(Console.ReadLine());
+            
+            switch (fieldToEdit)
+            {
+                case 1:
+                    Console.WriteLine("Введите новую фамилию:");
+                    Surname = Console.ReadLine();
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Введите новое имя:");
+                    Name = Console.ReadLine();
+                    break;
+                
+                case 3:
+                    Console.WriteLine("Введите новое отчество:");
+                    Patronymic = Console.ReadLine();
+                    break;
+                
+                case 4:
+                    Console.WriteLine("Введите новую дату рождения:");
+                    BirthDate = DateTime.Parse(Console.ReadLine());
+                    break;
+                
+                default:
+                    Console.WriteLine("Некорректный ввод");
+                    break;
+            }
+        }
         
+        public void Display()
+        {
+            Console.WriteLine($"ФИО: {Surname} {Name} {Patronymic}");
+            Console.WriteLine($"Дата рождения: {BirthDate.ToShortDateString()}");
+            Console.WriteLine($"Факультет: {Faculty}");
+            Console.WriteLine($"Курс: {Course}");
+            Console.WriteLine($"Группа: {Group}");
+        }
+
     }
 }
